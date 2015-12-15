bf3c7f996ad30f8184473b9d6752501648e5d85c
Sheetal Nandi <shkamat@microsoft.com>
2015-12-15 12:09:03 -0800

    tee-callback

--- tee-callback.ts
+++ tee-callback.ts
@@ -1,6 +1,7 @@
-module.exports = function(spyArray, cb) {
+function teeCallback<T>(spyArray: Array<T>, cb:(...rest: any[])=>T) {
   return function() {
     spyArray.push(cb.apply(this, arguments))
     return spyArray[spyArray.length-1]
   }
 }
+export = teeCallback;
