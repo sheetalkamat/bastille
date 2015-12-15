f17f348e8725cdb38ccef545f4a306b4df55d40c
Sheetal Nandi <shkamat@microsoft.com>
2015-12-15 11:52:53 -0800

    Vector is converted to TS

--- vector.ts
+++ vector.ts
@@ -1,13 +1,14 @@
-module.exports = {
-  normalize: function(vector) {
-    var l = this.length(vector)
-    return vector.map(function(p) { return p/l || 0 })
-  }
-, length: function(vector) {
-    return Math.sqrt(
-      vector.reduce(function(prev, current){
-        return prev + (current*current)
-      }, 0)
-    )
-  }
+class Vector extends Array<number> {
+    public normalize(vector: Vector) {
+        var l = this.length(vector)
+        return vector.map(function (p) { return p / l || 0 });
+    }
+    public length(vector: Vector) {
+        return Math.sqrt(
+            vector.reduce(function (prev, current) {
+                return prev + (current * current)
+            }, 0)
+        )
+    }
 }
+export = Vector;
